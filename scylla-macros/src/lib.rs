391655683b478795353c438013aff9dad1581042
cvybhu <cvybhu@selade.com>
2020-11-07 13:24:31 +0100

    implemented derive(FromRow)

--- scylla-macros/src/lib.rs
+++ scylla-macros/src/lib.rs
@@ -1,27 +1,50 @@
-extern crate proc_macro;
-
 use proc_macro::TokenStream;
-use quote::quote;
-use syn;
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(FromRow)]
-pub fn from_row_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    let ast = syn::parse(input).unwrap();
+pub fn from_row_derive(tokens_input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(tokens_input as DeriveInput);
 
-    // Build the trait implementation
-    impl_from_row(&ast)
-}
+    let struct_name = &input.ident;
 
-fn impl_from_row(ast: &syn::DeriveInput) -> TokenStream {
-    let name = &ast.ident;
-    let gen = quote! {
-        impl From<scylla::frame::response::result::Row> for #name {
+    // Generates tokens for field_name: field_type::from(vals_iter.next().unwrap_or(...)), ...
+    let set_fields = match &input.data {
+        Data::Struct(ref data) => {
+            match &data.fields {
+                Fields::Named(fields) => {
+                    fields.named.iter().map(|field| {
+                        let field_name = &field.ident;
+                        let field_type = &field.ty;
+
+                        quote_spanned! {field.span() =>
+                            #field_name: <#field_type as FromCQLVal<Option<CQLValue>>>::from(
+                                vals_iter
+                                .next().unwrap_or_else(
+                                || panic!("Row too short to convert to {}!", stringify!(#struct_name)))
+                            ),
+                        }
+                    })
+                },
+                _ => panic!("derive(FromRow) works only for structs with named fields. Tuples don't need derive.")
+            }
+        },
+        _ => panic!("derive(FromRow) works only on structs!")
+    };
+
+    let generated = quote! {
+        impl From<scylla::frame::response::result::Row> for #struct_name {
             fn from(row: scylla::frame::response::result::Row) -> Self {
-                unimplemented!();
+                let mut vals_iter = row.columns.into_iter();
+
+                use scylla::frame::response::result::{CQLValue, FromCQLVal};
+
+                #struct_name {
+                    #(#set_fields)*
+                }
             }
         }
     };
-    gen.into()
+
+    TokenStream::from(generated)
 }
